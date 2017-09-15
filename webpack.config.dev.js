46404cfad18bd2eee89639b69b32a8edb8949f0b
MrHuxu <hxtheone@gmail.com>
2017-09-15 13:08:14 +0800

    update webpack config to make the dashboard work

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const DashboardPlugin = require('webpack-dashboard/plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry : './client/index',
@@ -12,17 +13,23 @@ module.exports = {
   },
 
   resolve : {
-    extensions : ['.jsx', '.scss', '.js', '.json']
+    extensions : ['.jsx', '.js', '.json']
   },
 
   module : {
     loaders : [
       { test: /\.(js|jsx)$/, exclude: /node_modules/, loaders: ['babel-loader'] },
-      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
-      { test: /\.less$/, loaders: ['style-loader', 'css-loader', 'less-loader'] },
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loaders: ['url-loader?limit=10000&minetype=application/font-woff'] },
       { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loaders: ['file-loader'] },
-      { test: /\.(jpe?g|png|gif|svg)$/i, loaders: ['url?limit=10000!img?progressive=true'] }
+      { test: /\.(jpe?g|png|gif|svg)$/i, loaders: ['url?limit=10000!img?progressive=true'] },
+      {
+        test    : /\.css$/,
+        exclude : /\.modules\.css$/,
+        use     : ExtractTextPlugin.extract({
+          fallback : 'style-loader',
+          use      : 'css-loader'
+        })
+      },
     ]
   },
 
@@ -40,6 +47,10 @@ module.exports = {
 
   plugins : [
     new DashboardPlugin(),
+    new ExtractTextPlugin({
+      filename: `style.css`,
+      allChunks: true
+    }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV' : '"development"'
--- webpack.config.prd.js
+++ webpack.config.prd.js
@@ -1,9 +1,10 @@
 const path    = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry : [
-    './client/index' // Your appʼs entry point
+    './client/index'
   ],
 
   output : {
@@ -12,21 +13,31 @@ module.exports = {
   },
 
   resolve : {
-    extensions : ['.jsx', '.scss', '.js', '.json']
+    extensions : ['.jsx', '.js', '.json']
   },
 
   module : {
     loaders : [
       { test: /\.(js|jsx)$/, exclude: /node_modules/, loaders: ['babel-loader'] },
-      { test: /\.css$/, loaders: ['style-loader', 'css-loader'] },
-      { test: /\.less$/, loaders: ['style-loader', 'css-loader', 'less-loader'] },
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loaders: ['url-loader?limit=10000&minetype=application/font-woff'] },
       { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loaders: ['file-loader'] },
-      { test: /\.(jpe?g|png|gif|svg)$/i, loaders: ['url?limit=10000!img?progressive=true'] }
+      { test: /\.(jpe?g|png|gif|svg)$/i, loaders: ['url?limit=10000!img?progressive=true'] },
+      {
+        test    : /\.css$/,
+        exclude : /\.modules\.css$/,
+        use     : ExtractTextPlugin.extract({
+          fallback : 'style-loader',
+          use      : 'css-loader'
+        })
+      },
     ]
   },
 
   plugins : [
+    new ExtractTextPlugin({
+      filename: `style.css`,
+      allChunks: true
+    }),
     new webpack.optimize.UglifyJsPlugin({ minimize: true }),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV' : '"production"'
